2cecb2671f68b8d24a7edc84879b48d8066c4005
kartsridhar <contactkarthik34@gmail.com>
2019-11-09 13:48:24 +0000

    subtask 1 pretty much works

--- CW/subtask1/subtask1.cpp
+++ CW/subtask1/subtask1.cpp
@@ -44,7 +44,7 @@ int main(int argc, const char** argv)
 	f1_score();
 
 	// 4. Save Result Image
-	imwrite("dart15_detected.jpg", frame);
+	imwrite("detected.jpg", frame);
 
 	return 0;
 }
@@ -52,40 +52,40 @@ int main(int argc, const char** argv)
 /** Function to get the detected rectangles*/
 std::vector<Rect> detected_faces(int n) {
 	std::vector<Rect> result;
-	switch (n) 
+	switch (n)
 	{
-		case 4: result.push_back(Rect(342, 107, 147, 147)); break;
-		case 5: result.push_back(Rect(250, 164, 57, 57));
-				result.push_back(Rect(290, 242, 63, 63));
-				result.push_back(Rect(554, 244, 69, 69));
-				result.push_back(Rect(673, 246, 64, 64));
-				result.push_back(Rect(58, 249, 64, 64));
-				result.push_back(Rect(513, 177, 55, 55));
-				result.push_back(Rect(191, 214, 65, 65));
-				result.push_back(Rect(641, 184, 59, 59));
-				result.push_back(Rect(695, 599, 59, 59));
-				result.push_back(Rect(425, 231, 68, 68));
-				result.push_back(Rect(60, 135, 63, 63));
-				result.push_back(Rect(377, 190, 57, 57));
-				result.push_back(Rect(384, 400, 81, 81));
-				result.push_back(Rect(528, 482, 170, 170));
-				break;
-		case 13: result.push_back(Rect(204, 164, 53, 53));
-				result.push_back(Rect(412, 123, 122, 122));
-				break;
-		case 14: result.push_back(Rect(675, 219, 61, 61));
-				result.push_back(Rect(394, 326, 67, 67));
-				result.push_back(Rect(726, 190, 100, 100));
-				result.push_back(Rect(577, 135, 87, 87));
-				result.push_back(Rect(461, 216, 102, 102));
-				result.push_back(Rect(558, 52, 315, 315));
-				break;
-		case 15: result.push_back(Rect(548, 121, 54, 54));
-			result.push_back(Rect(65, 123, 86, 86));
-			result.push_back(Rect(63, 366, 100, 100));
-			result.push_back(Rect(124, 298, 147, 147));
-			break;
-		default: return result;
+	case 4: result.push_back(Rect(342, 107, 147, 147)); break;
+	case 5: result.push_back(Rect(250, 164, 57, 57));
+		result.push_back(Rect(290, 242, 63, 63));
+		result.push_back(Rect(554, 244, 69, 69));
+		result.push_back(Rect(673, 246, 64, 64));
+		result.push_back(Rect(58, 249, 64, 64));
+		result.push_back(Rect(513, 177, 55, 55));
+		result.push_back(Rect(191, 214, 65, 65));
+		result.push_back(Rect(641, 184, 59, 59));
+		result.push_back(Rect(695, 599, 59, 59));
+		result.push_back(Rect(425, 231, 68, 68));
+		result.push_back(Rect(60, 135, 63, 63));
+		result.push_back(Rect(377, 190, 57, 57));
+		result.push_back(Rect(384, 400, 81, 81));
+		result.push_back(Rect(528, 482, 170, 170));
+		break;
+	case 13: result.push_back(Rect(204, 164, 53, 53));
+		result.push_back(Rect(412, 123, 122, 122));
+		break;
+	case 14: result.push_back(Rect(675, 219, 61, 61));
+		result.push_back(Rect(394, 326, 67, 67));
+		result.push_back(Rect(726, 190, 100, 100));
+		result.push_back(Rect(577, 135, 87, 87));
+		result.push_back(Rect(461, 216, 102, 102));
+		result.push_back(Rect(558, 52, 315, 315));
+		break;
+	case 15: result.push_back(Rect(548, 121, 54, 54));
+		result.push_back(Rect(65, 123, 86, 86));
+		result.push_back(Rect(63, 366, 100, 100));
+		result.push_back(Rect(124, 298, 147, 147));
+		break;
+	default: return result;
 	}
 	return result;
 }
@@ -95,28 +95,28 @@ std::vector<Rect> ground_faces(int n) {
 	std::vector<Rect> result;
 	switch (n)
 	{
-		case 4: result.push_back(Rect(358, 141, 111, 101)); break;
-		case 5: result.push_back(Rect(69, 142, (119 - 69), (193 - 142)));
-				result.push_back(Rect(58, 260, (113 - 58), (310 - 260)));
-				result.push_back(Rect(198, 229, (247 - 198), (274 - 229)));
-				result.push_back(Rect(256, 174, (300 - 256), (216 - 174)));
-				result.push_back(Rect(297, 254, (344 - 297), (299 - 254)));
-				result.push_back(Rect(386, 196, (436 - 386), (239 - 196)));
-				result.push_back(Rect(437, 247, (481 - 437), (292 - 247)));
-				result.push_back(Rect(519, 187, (566 - 519), (229 - 187)));
-				result.push_back(Rect(565, 258, (613 - 565), (306 - 258)));
-				result.push_back(Rect(651, 195, (699 - 651), (238 - 195)));
-				result.push_back(Rect(681, 256, (731 - 681), (306 - 256)));
-				break;
-		case 13: result.push_back(Rect(427, 146, (513 - 427), (232 - 146))); break;
-		case 14: result.push_back(Rect(482, 238, (547 - 482), (305 - 482)));
-				result.push_back(Rect(738, 210, (815 - 738), (283 - 210)));
-				break;
-		case 15: result.push_back(Rect(74, 141, (123 - 74), (208 - 141)));
-				result.push_back(Rect(375, 115, (411 - 375), (184 - 115)));
-				result.push_back(Rect(540, 138, (596 - 203), (203 - 138)));
-				break;
-		default: return result;
+	case 4: result.push_back(Rect(358, 141, 111, 101)); break;
+	case 5: result.push_back(Rect(69, 142, (119 - 69), (193 - 142)));
+		result.push_back(Rect(58, 260, (113 - 58), (310 - 260)));
+		result.push_back(Rect(198, 229, (247 - 198), (274 - 229)));
+		result.push_back(Rect(256, 174, (300 - 256), (216 - 174)));
+		result.push_back(Rect(297, 254, (344 - 297), (299 - 254)));
+		result.push_back(Rect(386, 196, (436 - 386), (239 - 196)));
+		result.push_back(Rect(437, 247, (481 - 437), (292 - 247)));
+		result.push_back(Rect(519, 187, (566 - 519), (229 - 187)));
+		result.push_back(Rect(565, 258, (613 - 565), (306 - 258)));
+		result.push_back(Rect(651, 195, (699 - 651), (238 - 195)));
+		result.push_back(Rect(681, 256, (731 - 681), (306 - 256)));
+		break;
+	case 13: result.push_back(Rect(427, 146, (513 - 427), (232 - 146))); break;
+	case 14: result.push_back(Rect(482, 238, (547 - 482), (305 - 482)));
+		result.push_back(Rect(738, 210, (815 - 738), (283 - 210)));
+		break;
+	case 15: result.push_back(Rect(74, 141, (123 - 74), (208 - 141)));
+		result.push_back(Rect(375, 115, (411 - 375), (184 - 115)));
+		result.push_back(Rect(540, 138, (596 - 203), (203 - 138)));
+		break;
+	default: return result;
 	}
 	return result;
 }
@@ -165,7 +165,7 @@ void f1_score()
 		for (g = ground.begin(); g != ground.end(); ++g)
 		{
 			if ((*g & *d) == *g)
-			{	
+			{
 				actual_hits += 1;
 				continue;
 			}
@@ -180,4 +180,4 @@ void f1_score()
 	std::cout << "Actual Hits: " << actual_hits << std::endl;
 	std::cout << "TPR: " << tpr << std::endl;
 	//std::cout << "F1-Score: " << std::to_string(f1) << std::endl;
-}	
+}
