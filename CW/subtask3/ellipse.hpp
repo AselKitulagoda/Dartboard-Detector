830b9e9827d2d80d1180d7a8c876eb805ff8ba02
kartsridhar <contactkarthik34@gmail.com>
2019-11-23 23:07:26 +0000

    added function to get minimum bounding rect from ellipse

--- CW/subtask3/ellipse.hpp
+++ CW/subtask3/ellipse.hpp
@@ -12,6 +12,8 @@
 using namespace std;
 using namespace cv;
 
+#define PI 3.1415
+
 std::vector<RotatedRect> ellipse_detector(Mat mag_img, Mat dir_img) 
 {
     // Edge detection on magnitude img
@@ -35,6 +37,53 @@ std::vector<RotatedRect> ellipse_detector(Mat mag_img, Mat dir_img)
         }
     }
     return ellipses;
+}
+
+std::vector<Rect> convert_rotated_rect(std::vector<RotatedRect> ellipses)
+{
+    // https://stackoverflow.com/questions/32920419/opencv-minimum-upright-bounding-rect-of-a-rotatedrect
+    std::vector<Rect> bounding_rectangles;
+
+    for(int i = 0; i < ellipses.size(); i++)
+    {
+        float degree = ellipses[i].angle * PI/180;
+        float majorAxe = ellipses[i].size.width / 2;
+        float minorAxe = ellipses[i].size.height / 2;
+        float x = ellipses[i].center.x;
+        float y = ellipses[i].center.y;
+        float c_degree = cos(degree);
+        float s_degree = sin(degree);
+        float t1 = atan(-(majorAxe * s_degree) / (minorAxe * c_degree));
+        float c_t1 = cos(t1);
+        float s_t1 = sin(t1);
+        float w1 = majorAxe * c_t1 * c_degree;
+        float w2 = minorAxe * s_t1 * s_degree;
+        float maxX = x + w1-w2;
+        float minX = x - w1+w2;
+
+        t1 = atan((minorAxe * c_degree) / (majorAxe * s_degree));
+        c_t1 = cos(t1);
+        s_t1 = sin(t1);
+        w1 = minorAxe * s_t1 * c_degree;
+        w2 = majorAxe * c_t1 * s_degree;
+        float maxY = y + w1+w2;
+        float minY = y - w1-w2;
+        if (minY > maxY)
+        {
+            float temp = minY;
+            minY = maxY;
+            maxY = temp;
+        }
+        if (minX > maxX)
+        {
+            float temp = minX;
+            minX = maxX;
+            maxX = temp;
+        }
+        Rect rect(minX, minY, maxX-minX+1, maxY-minY+1);
+        bounding_rectangles.push_back(rect);
+    }
+    return bounding_rectangles;
 }
 
 void draw_ellipses(Mat original_img, std::vector<RotatedRect> ellipses)
